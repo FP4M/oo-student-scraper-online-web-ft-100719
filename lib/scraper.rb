eaa7f4a087007ae2e959f39810ba2dd590802c60
Prince Charles <princecharles35@yahoo.com>
2019-11-05 17:37:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,20 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
+
 
 class Scraper
 
   def self.scrape_index_page(index_url)
+    puts 'these are the best trail in the philladelphia area.'
+    puts " "
+    puts '-------------'
+    puts ' '
     
+    
+    html = open("https://www.alltrails.com/")
+    doc = nokogiri::HTML(html)
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
